23f00aa291448910863c8c8493798af05dd9c3a5
Sudhanwa <sudhanwa.kaveeshwar@gmail.com>
2026-04-18 00:58:41 +0530

    added transformers

--- src/core/constants/registry.py
+++ src/core/constants/registry.py
@@ -2,6 +2,20 @@ from enum import Enum
 from typing import Callable, Dict
 
 from src.core.models.linear import build_model as build_linear_model
+from src.core.context.policies import (
+    RenamePolicy,
+    TypeCastPolicy,
+    MissingValuePolicy,
+    EncodingPolicy,
+    ScalingPolicy,
+)
+from src.core.pipelines.transformers import (
+    RenameColumnsTransformer,
+    MissingValueTransformer,
+    CleaningTransformer,
+    FilterTransformer,
+    ColumnTransformerBuilder,
+)
 
 import pandas as pd
 
@@ -17,9 +31,37 @@ DATA_READER_FUNCTIONS: Dict[str, Callable[[str], pd.DataFrame]] = {
 }
 
 
-class PreprocessingType(str, Enum):
-    rename_columns = "rename_columns"
+class PreprocessingRegistry(str, Enum):
+    rename_columns = "rename"
     type_cast = "type_cast"
-    missing_values = "missing_values"
+    missing_values = "missing"
     encoding = "encoding"
     scaling = "scaling"
+    cleaning = "cleaning"
+    filter = "filter"
+
+    @property
+    def policy_class(self):
+        mapping = {
+            PreprocessingRegistry.rename_columns: RenamePolicy,
+            PreprocessingRegistry.type_cast: TypeCastPolicy,
+            PreprocessingRegistry.missing_values: MissingValuePolicy,
+            PreprocessingRegistry.encoding: EncodingPolicy,
+            PreprocessingRegistry.scaling: ScalingPolicy,
+            PreprocessingRegistry.cleaning: None,
+            PreprocessingRegistry.filter: None,
+        }
+        return mapping.get(self)
+
+    @property
+    def transformer_class(self):
+        mapping = {
+            PreprocessingRegistry.rename_columns: RenameColumnsTransformer,
+            # PreprocessingType.type_cast: None,  # Handled via custom logic or future transformer
+            PreprocessingRegistry.missing_values: MissingValueTransformer,
+            PreprocessingRegistry.encoding: ColumnTransformerBuilder,
+            PreprocessingRegistry.scaling: ColumnTransformerBuilder,
+            PreprocessingRegistry.cleaning: CleaningTransformer,
+            PreprocessingRegistry.filter: FilterTransformer,
+        }
+        return mapping.get(self)
